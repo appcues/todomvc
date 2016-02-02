b59b5004a29e5c483724c1b875122d9a9ca84166
Ray Di Ciaccio <ray@appcues.com>
2016-02-02 14:07:55 -0500

    Remove API persistence code.

--- examples/angularjs/js/app.js
+++ examples/angularjs/js/app.js
@@ -14,11 +14,8 @@ angular.module('todomvc', ['ngRoute', 'ngResource'])
 			templateUrl: 'todomvc-index.html',
 			resolve: {
 				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
+					todoStorage.get();
+					return todoStorage;
 				}
 			}
 		};
